99c1fcdf349924a8972c7e5d9be821b7997404b5
Matt Cole <mattcole@riserobotics.com>
2022-12-27 14:48:30 -0500

    reworked the whole damn thing

--- secret-santa.py
+++ secret-santa.py
@@ -1,61 +1,88 @@
 #!/usr/bin/env python3
 
-import pandas as pd
-import numpy as np
 import argparse as ap
+import csv
+import random
 import smtplib, ssl, getpass
 import yagmail
 
+def secret_santa(participants, debug):
+    # Shuffle the list of participants to randomly assign gift recipients
+    names = list(participants.keys())
+    recipients = list(participants.keys())
+
+    if debug:
+        print(names)
+        print(recipients)
+    random.shuffle(names)
+    random.shuffle(recipients)
+
+    # Assign gift recipients
+    assignments = {}
+    for i, name in enumerate(names):
+        if debug:
+            print("i is", i, "name is ", name, "recipient is", recipients[i] )
+        assignments[name] = recipients[i]
+
+    return assignments
+
+def secret_santas_collide(secret_santa_assigns, debug):
+    for name in secret_santa_assigns.keys():
+        if debug:
+            print(name, secret_santa_assigns[name])
+        if name == secret_santa_assigns[name]:
+            return True
+    return False
+        
+        
+
 def main():
+    debug = False
+    secret_santa_assigns = {}
+
     parser = ap.ArgumentParser(description='Secret Santa')
-    parser.add_argument("-f", "--file", help="input file, names and emails", type=ap.FileType('r'))
-    parser.add_argument("-v", "--verbosity", action="store_true", help="increase output verbosity")
-    parser.add_argument("-e", "--email", help="email address to send secret santa notices from")
+    parser.add_argument("-f", "--file", help="input file, names and emails")
+    parser.add_argument("-d", "--debug", action="store_true", help="increase output verbosity; print list of gifters/recipients; send no emails")
+    parser.add_argument("-e", "--email", required=True, help="email address to send secret santa notices from")
+    parser.add_argument("-p", "--password", required=True, help="application-specific password for gmail SMTP access")
     args = parser.parse_args()
-    #if args.verbosity:
-    #    print(args.file.readlines())
-    in_data = get_list(args)
 
-    if args.verbosity:
+    # Read the CSV file and store the name and email in a dictionary
+    participants = {}
+    with open(args.file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if args.debug:
+                print("new row")
+                print(row)
+            participants[row['Name']] = row['Email']
+
+    if debug:
+        print("Participant list as read in")
+        print(participants)
+
+    iter_count = 0
+    collisions = True
+    while collisions:
+        iter_count = iter_count+1
+        print("Randomizing.... Attempt", iter_count)
+        secret_santa_assigns = secret_santa(participants, args.debug)
+        collisions = secret_santas_collide(secret_santa_assigns, args.debug)
+
+
+    if args.debug:
+        print("Here is the assignment dict")
+        print(secret_santa_assigns)
         print("---")
-        in_data.info()
-        print("xxx")
-        print(in_data.count())
-
-    shuffled_list = in_data.sample(frac=1).reset_index(drop=True)
-    recips = shuffled_list.iloc[:,0]
-    length = recips.count()
-
-    if args.verbosity:
-        print("---")
-        shuffled_list.info()
-        print("***")
-        print(recips)
-
-    password = getpass.getpass(prompt='Password: ', stream=None)
-
-    if args.verbosity:
-        print("###")
-        print(length)
-        for idx, row in shuffled_list.iterrows():
-            print(idx, row['Name'], row['Email'], recips[(idx+1)%length])
-
-    if not args.verbosity:
-        yag = yagmail.SMTP(args.email)
-        for idx, row in shuffled_list.iterrows():
-
-            yag.send(row['Email'], 'Your secret santa recipient is inside', "Your gift goes to: " + recips[(idx+1)%length])
+        for name in secret_santa_assigns.keys():
+            print(participants[name], name + ', your secret santa recipient is inside', "Your gift goes to: " + secret_santa_assigns[name])
+        
+    if not args.debug:
+        yag = yagmail.SMTP(args.email, args.password)
+        for name in secret_santa_assigns.keys():
+            yag.send(participants[name], name + ', your secret santa recipient is inside', "Your gift goes to: " + secret_santa_assigns[name])
 
     return
-
-def get_list(args):
-    df = pd.read_csv(args.file, header=0)
-    dataset = (df[['Name', 'Email']])
-
-    if args.verbosity:
-        print("---")
-        print(dataset)
-    return(dataset)
 
 if __name__ == "__main__":
     main()
