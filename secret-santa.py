6cd142225239ddecfc01eb7a88cb27b00920762e
Matthew Cole <matthew@lightmatter.co>
2024-11-19 13:52:08 -0500

    pre-seeded pairings now accomplished

--- secret-santa.py
+++ secret-santa.py
@@ -5,17 +5,26 @@ import csv
 import random
 import smtplib, ssl, getpass
 import yagmail
+import time
+from pprint import pprint
 
-def secret_santa_shuffle(participants, debug):
+def secret_santa_shuffle(participants, preseed_assignments, debug):
     # Shuffle the list of participants to randomly assign gift recipients
     names = list(participants.keys())
     recipients = list(participants.keys())
+
+    # Remove pre-seeded pairs from the lists
+    for name, recipient in preseed_assignments.items():
+        if name in names:
+            names.remove(name)
+        if recipient in recipients:
+            recipients.remove(recipient)
 
     random.shuffle(names)
     random.shuffle(recipients)
 
     # Assign gift recipients
-    assignments = {}
+    assignments = preseed_assignments.copy()
     for i, name in enumerate(names):
         if debug:
             print("i is", i, "name is ", name, "recipient is", recipients[i] )
@@ -34,8 +43,14 @@ def secret_santas_collide(secret_santa_assigns, year1, year2, debug):
         if name in year2.keys() and secret_santa_assigns[name] == year2[name]:
             return True            
     return False
-        
-        
+
+def parse_preseed(preseed_str):
+    preseed_assignments = {}
+    pairs = preseed_str.split(',')
+    for pair in pairs:
+        name, recipient = pair.split(':')
+        preseed_assignments[name.strip()] = recipient.strip()
+    return preseed_assignments
 
 def main():
     debug = False
@@ -50,7 +65,21 @@ def main():
     parser.add_argument("-d", "--debug", action="store_true", help="increase output verbosity; print list of gifters/recipients; send no emails")
     parser.add_argument("-e", "--email", help="email address to send secret santa notices from")
     parser.add_argument("-p", "--password", help="application-specific password for gmail SMTP access")
+    parser.add_argument("-s", "--preseed", required=False, help="pre-seeded name/recipient pairings in the format 'name1:recipient1,name2:recipient2,...'")
+
     args = parser.parse_args()
+
+    if args.debug:
+        debug = True
+
+    if args.preseed:
+        print("Naughty Santa Mode engaged")
+        secret_santa_preseed = parse_preseed(args.preseed)
+        if debug:
+            print("Pre-seeded assignments:", secret_santa_preseed)
+
+    # Seed the random number generator
+    random.seed(time.time())
 
     # Read the CSV file and store the name and email in a dictionary
     participants = {}
@@ -64,35 +93,26 @@ def main():
             reader1 = csv.DictReader(csvfile)
             for row in reader1:
                 last_year_assigns[row['Name']] = row['Giftee']
-            print("2022:", last_year_assigns)
+            print("last year:", last_year_assigns)
 
     if args.old2:
         with open(args.old2) as csvfile:
             reader2 = csv.DictReader(csvfile)
             for row in reader2:
                 other_year_assigns[row['Name']] = row['Giftee']
-            print("2021", other_year_assigns)
+            print("two years ago:", other_year_assigns)
 
     iter_count = 0
     collisions = True
     while collisions:
         iter_count = iter_count+1
         print("Randomizing.... Attempt", iter_count)
-        secret_santa_assigns = secret_santa_shuffle(participants, args.debug)
+        secret_santa_assigns = secret_santa_shuffle(participants, secret_santa_preseed, args.debug)
         collisions = secret_santas_collide(secret_santa_assigns, last_year_assigns, other_year_assigns, args.debug)
 
-
     if args.debug:
-        print("Your theoretical assignment list:")
-        for name in secret_santa_assigns.keys():
-            print(participants[name], name + ', your secret santa recipient is inside', "Your gift goes to: " + secret_santa_assigns[name])
-        
-    if not args.debug:
-        yag = yagmail.SMTP(args.email, args.password)
-        for name in secret_santa_assigns.keys():
-            yag.send(participants[name], name + ', your secret santa recipient is inside', "Your gift goes to: " + secret_santa_assigns[name])
-
-    return
+        print("Final assignments:")
+        pprint(secret_santa_assigns)
 
 if __name__ == "__main__":
     main()
